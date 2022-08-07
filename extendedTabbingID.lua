9f28278eef0bb1f02113335c79477aeb4b4a8f17
Martin Eller <martin@eller-essen.de>
2022-08-07 14:36:54 +0200

    Update extendedTabbingID.lua

--- extendedTabbingID.lua
+++ extendedTabbingID.lua
@@ -5,7 +5,7 @@
 -- 
 
 source(g_currentModDirectory.."tools/gmsDebug.lua")
-GMSDebug:init(g_currentModName, true, 2)
+GMSDebug:init(g_currentModName)
 GMSDebug:enableConsoleCommands()
 
 ExtendedTabbingID = {}
